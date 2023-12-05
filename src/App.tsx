1708edc85ba164398ac470bef80f6c0d2b183934
Isola513I <okuma530@gmail.com>
2023-12-05 23:30:39 +0700

    adding-search-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Nav from "./component/Nav";
 import "../dist/output.css";
+import SearchBar from "./component/SearchBar";
 
 function App() {
   return (
     <>
-      <Nav />
-      <p>
-        
-      </p>
+      <Nav/>
+      <SearchBar/>
+      
     </>
   );
 }
