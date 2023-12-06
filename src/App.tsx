b3b1a7289ff47b46428db64e5a9c68fb12187650
Nattapat Lamnui <okuma530@gmail.com>
2023-12-06 14:52:42 +0700

    test
    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Nav from "./component/Nav";
-import "../dist/output.c
+import "../dist/output.css";
 import SearchBar from "./component/SearchBar";
 import HeroSection from "./component/HeroSection";
 import Container from "./component/Container";
-import ShopSectionBox from "./component/ShopSectionBox";
-import HeroSection from "./component/HeroSection";  
 import Footer from "./component/Footer.tsx";
 
 function App() {
