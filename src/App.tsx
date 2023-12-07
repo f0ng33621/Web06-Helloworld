10fab64e76ff6ad6550f33f7931be62a71c56e53
Srisuphak <Srisuphak.2547@gmail.com>
2023-12-07 17:15:37 +0700

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Nav from "./component/Nav";
 import "../dist/output.css";
 import Footer from "./component/Footer.tsx";
-import BuyNow from "./component/BuyNow.tsx";
 
 function App() {
   return (
